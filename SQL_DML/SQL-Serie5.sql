9b76722ff3723b8e516910fbd75f7af7d3943f94
Facundo Vergara <fakumax@gmail.com>
2019-05-10 18:39:00 -0300

    V1

---
+++ SQL_DML/SQL-Serie5.sql
@@ -0,0 +1,27 @@
+/*5. Ordenación
+
+5.1. Listar los empleados ordenados por apellido, por nombre y por inicial del segundo
+nombre. */
+
+use editorial
+
+select empleados.apellido, empleados.nombre, empleados.inicial_segundo_nombre
+from empleados
+order by empleados.apellido, empleados.nombre, empleados.inicial_segundo_nombre
+
+--5.2. Listar los títulos pertenecientes al género ‘business’. Por cada fila, listar el id, el título
+--y el precio. Ordenar los datos por precio en forma descendente e id de artículo en
+--forma ascendente.
+
+select titulos.titulo_id,titulos.titulo,titulos.precio
+from titulos
+where titulos.genero='business'
+order by titulos.precio desc, titulos.titulo_id asc
+
+--5.3. Informar la venta más importante con forma de pago a 60 días. Mostrar el almacén, el
+--número de orden, la fecha de la factura y el título.
+
+select almacenes.almacen_nombre,ventas.numero_orden,ventas.fecha_orden,titulos.titulo,ventas.forma_pago, '+ Importante'=titulos.precio*ventas.cantidad
+from ventas, almacenes,titulos
+where almacenes.almacen_id = ventas.almacen_id  and titulos.titulo_id= ventas.titulo_id and 
+ventas.forma_pago='60 días' 
